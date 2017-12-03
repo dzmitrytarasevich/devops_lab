5cb4dfa51c11382ef2b89d99bd056efcff20d71c
dzmitrytarasevich <dmitrytarasevich@mail.ru>
2017-12-04 00:47:40 +0300

    Changed task2-2.py file

--- homework2/task2-2.py
+++ homework2/task2-2.py
@@ -1,7 +1,9 @@
-list1 = ['key1', 'key2']
-list2 = ['value1']
-dict(filter(lambda x: x[0] is not None, map(lambda key, value: (key, value), list1, list2)))
-{'key2': None, 'key1': 'value1'}
-list2 = ['value1', 3, 6]
-dict(filter(lambda x: x[0] is not None, map(lambda key, value: (key, value), list1, list2)))
-{'key2': 3, 'key1': 'value1'}
+def main():
+
+    list1 = ['key1', 'key2', 'key3', 'key4']
+    #list1 = ['key1', 'key2']
+    list2 = ['value1', 'value2', 'value3']
+    return dict((n,m) for n,m in map(None, list1, list2) if n is not None)
+
+if __name__ == '__main__':
+   print main()
