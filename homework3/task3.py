d16674493e3728bfbd43a0c8ab5aa80728c59dd0
dzmitrytarasevich <dmitrytarasevich@mail.ru>
2017-12-05 15:51:39 +0300

    Task3.py modified, task3 done

--- homework3/task3.py
+++ homework3/task3.py
@@ -1,16 +1,42 @@
 import psutil
 from datetime import datetime
+import configparser
+import time
 
-cpu = psutil.cpu_percent(interval=1)
-vm = psutil.virtual_memory()
-disk = psutil.disk_io_counters(perdisk=False)
-net = psutil.net_io_counters(pernic=False)
-memory = psutil.swap_memory()
-time = datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M:%S")
-with open('/opt/log.txt', "a") as log:
-    log.write(time + " | " + "CPU load: " + str(cpu) + "%" + '\n')
-    log.write(time + " | " + "Memory usage: " + str(memory.percent) + "%" + '\n')
-    log.write(time + " | " + "VM usage: " + str(vm.percent) + "%" + '\n')
-    log.write(time+" | "+"I/O info: "+"Read count: "+str(disk.read_count)+" Write count: "+str(disk.write_count)+'\n')
-    log.write(time + " | " + "Net (bytes sent/recv): " + str(net.bytes_sent) +"/" + str(net.bytes_recv) + '\n')
-    log.write("---------------------------------------------" + '\n')
+
+def get_params():
+    config = configparser.ConfigParser()
+    config.read('settings.ini')
+
+    interval_min = config['common']['interval']
+    data_type = config['common']['output']
+
+    return [int(interval_min), str(data_type)]
+
+
+if __name__ == '__main__':
+
+    interval = get_params()[0]
+    type_data = get_params()[1]
+
+    i = 0
+    while True:
+        i += 1
+        time.sleep(interval * 60)
+
+        cpu = psutil.cpu_percent()
+        vm = psutil.virtual_memory()
+        disk = psutil.disk_io_counters(perdisk=False)
+        net = psutil.net_io_counters(pernic=False)
+        memory = psutil.swap_memory()
+        time_format = datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M:%S")
+
+        with open('snapshot.log', "a+") as log:
+            log.write("| SNAPSHOT " + str(i) + ": " + time_format + '\n')
+            log.write("| " + "CPU load: " + str(cpu) + "%" + '\n')
+            log.write("| " + "Memory usage: " + str(memory.percent) + "%" + '\n')
+            log.write("| " + "VM usage: " + str(vm.percent) + "%" + '\n')
+            log.write("| " + "I/O (read/write): " + str(disk.read_count) + " / " + str(disk.write_count) + '\n')
+            log.write("| " + "Net (bytes sent/recv): " + str(net.bytes_sent) + " / " + str(net.bytes_recv))
+            log.write("\n---------------------------------------------\n")
+            log.close()
