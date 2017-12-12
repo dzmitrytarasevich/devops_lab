98e4ebe3127d34a29b9ffd21e7100c12cf6c2880
dzmitrytarasevich <dmitrytarasevich@mail.ru>
2017-12-12 11:51:39 +0300

    Deleted

--- homework4/lecture6.py
+++
@@ -1,8 +0,0 @@
-# debug.py
-# import pdb
-a = "aaa"
-# pdb.set_trace()
-b = "bbb"
-c = "ccc"
-final = a + b + c
-print(final)
--- homework4/python3.5.4/lib/python3.5/_collections_abc.py
+++
@@ -1,1 +0,0 @@
-/root/.pyenv/versions/3.5.4/lib/python3.5/_collections_abc.py
